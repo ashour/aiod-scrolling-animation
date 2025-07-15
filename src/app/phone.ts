87505bc58e39a183e49982934047b8b131f5614b
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-07-15 16:49:48 +0100

    feat: custom object animations

--- src/app/phone.ts
+++ src/app/phone.ts
@@ -1,6 +1,7 @@
 import engine from "@/engine";
 import time from "@/engine/system/time";
 import worldObject from "@/engine/world/worldObject";
+import * as THREE from "three";
 import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 type PhoneProps = {
@@ -11,19 +12,23 @@ type PhoneProps = {
 export function phone(): WorldObject<PhoneProps> {
   let _isFloatingEnabled: boolean = true;
 
-  const gltf = engine.resource<GLTF>("phoneModel");
-  const _phone = gltf.scene;
+  const _gltf = engine.resource<GLTF>("phoneModel");
+  const _phone = _gltf.scene;
 
   _phone.scale.set(80, 80, 80);
   _phone.position.set(1, -3, 4.3);
   _phone.rotation.set(0.2, 1.1, 0.9);
+
+  const _mixer = new THREE.AnimationMixer(_phone);
 
   let _baseY = _phone.position.y;
   let _baseRotationX = _phone.rotation.x;
   let _baseRotationZ = _phone.rotation.z;
 
   return worldObject(_phone, {
-    update(_deltaTime: number) {
+    update(deltaTime: number) {
+      _mixer.update(deltaTime);
+
       if (!_isFloatingEnabled) {
         return;
       }
@@ -41,6 +46,7 @@ export function phone(): WorldObject<PhoneProps> {
       folder.add(_phone.rotation, "x").min(-10).max(10).step(0.1).name("rotX");
       folder.add(_phone.rotation, "y").min(-10).max(10).step(0.1).name("rotY");
       folder.add(_phone.rotation, "z").min(-10).max(10).step(0.1).name("rotZ");
+      folder.add(this, "playAnimations");
       return folder;
     },
 
@@ -55,6 +61,16 @@ export function phone(): WorldObject<PhoneProps> {
         _baseRotationX = _phone.rotation.x;
         _baseRotationZ = _phone.rotation.z;
       }
+    },
+
+    playAnimations() {
+      _gltf.animations.forEach((clip) => {
+        const action = _mixer.clipAction(clip);
+        action.reset();
+        action.setLoop(THREE.LoopOnce, 1);
+        action.clampWhenFinished = true;
+        action.play();
+      });
     },
   });
 }
