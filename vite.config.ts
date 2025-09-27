cbf1561d422bfa8b12c235433533854e279d0d94
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-09-27 13:48:19 +0100

    build: remove gzip compression

--- vite.config.ts
+++ vite.config.ts
@@ -62,12 +62,7 @@ export default defineConfig({
     }),
 
     compression({
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-    compression({
       algorithm: "brotliCompress",
-      ext: ".br",
     }),
 
     visualizer({
